c5e3c4cfcb2a36ffd11634c12e0d682fae73b910
barkhayadav12 <143165059+barkhayadav12@users.noreply.github.com>
2024-01-24 20:23:23 +0530

    Update Adopt.jsx

--- FRONTEND/src/Adopt.jsx
+++ FRONTEND/src/Adopt.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import Golu from './Images/golu.jpeg'
+import Minnie from './Images/minnie.jpeg'
 const Adopt = () => {
   const [info, setInfo] = useState([]);
   useEffect(() => {
